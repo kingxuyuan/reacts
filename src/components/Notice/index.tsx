4b93a28b356d8cf55025e853652bd949d2f7608e
lemon <lemon@gmail.com>
2022-02-24 14:54:46 +0800

    1

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 大侠传授两招吧
  * @Date: 2022-02-05 14:26:30
  * @LastEditors: 大侠传授两招吧
- * @LastEditTime: 2022-02-05 14:51:55
+ * @LastEditTime: 2022-02-24 14:54:34
  * @Description: 
  */
 import { FC, useState } from 'react';
@@ -24,10 +24,10 @@ const Notice: FC<indexProps> = (props) => {
     return (
         <div className="notice">
             <Tabs defaultActiveKey={active} onChange={callback}>
-                <TabPane tab="提现(10)" key='1'>
+                <TabPane tab="card1(10)" key='1'>
                     Content of Tab Pane 1
                 </TabPane>
-                <TabPane tab="充值(20)" key='2'>
+                <TabPane tab="card2(20)" key='2'>
                     Content of Tab Pane 2
                 </TabPane>
             </Tabs>
@@ -35,7 +35,7 @@ const Notice: FC<indexProps> = (props) => {
             <div className="notice-btns">
                 <div>
                     清空
-                    <span>{parseInt(active) === 1 ? '提现' : '充值'}</span>
+                    <span>{parseInt(active) === 1 ? 'card1' : '充值'}</span>
                 </div>
                 <div>查看更多</div>
             </div>
